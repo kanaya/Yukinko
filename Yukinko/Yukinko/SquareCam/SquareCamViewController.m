a2b4f997f09a230bcb5a4fee19e7cf1742d1884e
Ichi Kanaya <kanaya@pineapple.cc>
2015-09-11 20:26:23 +0900

    Opaque movie.

--- Yukinko/Yukinko/SquareCam/SquareCamViewController.m
+++ Yukinko/Yukinko/SquareCam/SquareCamViewController.m
@@ -140,7 +140,7 @@ static CGFloat DegreesToRadians(CGFloat degrees) {
   // playerLayer.bounds = self.view.bounds;
   // NSLog(@"frame = (%f, %f, %f, %f); bounds = (%f, %f, %f, %f)", self.view.frame.origin.x, self.view.frame.origin.y, self.view.frame.size.width, self.view.frame.size.height, self.view.bounds.origin.x, self.view.bounds.origin.y, self.view.bounds.size.width, self.view.bounds.size.height);
 
-  UIImage *maskImage = [UIImage imageNamed: @"halfmask.png"];
+  UIImage *maskImage = [UIImage imageNamed: @"mask.png"];
   CALayer *maskLayer = [CALayer layer];
   maskLayer.contents = (id)maskImage.CGImage;
   maskLayer.frame = playerLayer.frame;
