60fb400edb3993e568118082cef2936cc7101026
Ichi Kanaya <kanaya@pineapple.cc>
2015-09-11 15:17:19 +0900

    Alpha-masked movie player.

--- Test/avmovieplayer/avmovieplayer/ViewController.m
+++ Test/avmovieplayer/avmovieplayer/ViewController.m
@@ -24,7 +24,18 @@
   NSLog(@"%@", movieURL);
   AVPlayer *player = [AVPlayer playerWithURL: movieURL];
   AVPlayerLayer *playerLayer = [AVPlayerLayer playerLayerWithPlayer: player];
-  playerLayer.bounds = self.view.bounds;
+  playerLayer.frame = self.view.frame;
+  // playerLayer.bounds = self.view.bounds;
+  NSLog(@"frame = (%f, %f, %f, %f); bounds = (%f, %f, %f, %f)", self.view.frame.origin.x, self.view.frame.origin.y, self.view.frame.size.width, self.view.frame.size.height, self.view.bounds.origin.x, self.view.bounds.origin.y, self.view.bounds.size.width, self.view.bounds.size.height);
+
+  UIImage *maskImage = [UIImage imageNamed: @"testmask.png"];
+  CALayer *maskLayer = [CALayer layer];
+  maskLayer.contents = (id)maskImage.CGImage;
+  maskLayer.frame = playerLayer.frame;
+  // maskLayer.position = CGPointZero;
+
+  playerLayer.mask = maskLayer;
+
   CALayer *rootLayer = self.view.layer;
   [rootLayer addSublayer: playerLayer];
   [player play];
