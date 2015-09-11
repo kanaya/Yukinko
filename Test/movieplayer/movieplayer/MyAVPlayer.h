78c8e92bbbb15066a5aa6a1fac62145962c1c773
Ichi Kanaya <kanaya@pineapple.cc>
2015-09-11 13:52:34 +0900

    Added AVPlayerDemo.

---
+++ Test/movieplayer/movieplayer/MyAVPlayer.h
@@ -0,0 +1,13 @@
+//
+//  MyAVPlayer.h
+//  movieplayer
+//
+//  Created by Ichi Kanaya on 9/11/15.
+//  Copyright © 2015 Pinapple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyAVPlayer : AVPlayerViewController
+
+@end
---
+++ Test/movieplayer/movieplayer/MyAVPlayer.m
@@ -0,0 +1,13 @@
+//
+//  MyAVPlayer.m
+//  movieplayer
+//
+//  Created by Ichi Kanaya on 9/11/15.
+//  Copyright © 2015 Pinapple. All rights reserved.
+//
+
+#import "MyAVPlayer.h"
+
+@implementation MyAVPlayer
+
+@end
