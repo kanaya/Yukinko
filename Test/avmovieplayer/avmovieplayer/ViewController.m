b5d3a8e77523f3cc99dafc6cd2f0b2d95207b4b6
Ichi Kanaya <kanaya@pineapple.cc>
2015-09-11 14:12:15 +0900

    AVPlayer demo worked.

--- Test/avmovieplayer/avmovieplayer/ViewController.m
+++ Test/avmovieplayer/avmovieplayer/ViewController.m
@@ -6,6 +6,7 @@
 //  Copyright © 2015 Pinapple. All rights reserved.
 //
 
+#import <AVFoundation/AVFoundation.h>
 #import "ViewController.h"
 
 @interface ViewController ()
@@ -17,11 +18,22 @@
 - (void)viewDidLoad {
   [super viewDidLoad];
   // Do any additional setup after loading the view, typically from a nib.
+
+  NSBundle *mainBundle = [NSBundle mainBundle];
+  NSURL *movieURL = [mainBundle URLForResource: @"alpaca-background" withExtension: @"mov"];
+  NSLog(@"%@", movieURL);
+  AVPlayer *player = [AVPlayer playerWithURL: movieURL];
+  AVPlayerLayer *playerLayer = [AVPlayerLayer playerLayerWithPlayer: player];
+  playerLayer.bounds = self.view.bounds;
+  CALayer *rootLayer = self.view.layer;
+  [rootLayer addSublayer: playerLayer];
+  [player play];
 }
 
 - (void)didReceiveMemoryWarning {
   [super didReceiveMemoryWarning];
   // Dispose of any resources that can be recreated.
+
 }
 
 @end
